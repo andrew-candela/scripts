a3502b4e0297639b64251c8d8f8e609a7e1d437a
andrew <andrewpcandela@gmail.com>
2016-08-01 18:15:23 -0700

    script to log into the MM api

---
+++ python/utils/api_call.py
@@ -0,0 +1,14 @@
+import requests
+import py_utils as pu
+
+def mm_api_login():
+	"""log into the mediamath API"""
+	creds=pu.load_credentials()['mediamath_api']
+	payload={'user':creds['user'],'password':creds['password'],'api_key':creds['key']}
+	MM_LOGIN_URL='https://api.mediamath.com/api/v2.0/login'
+	s = requests.session()
+	s.post(MM_LOGIN_URL, data=payload)
+	
+	return s
+
+
