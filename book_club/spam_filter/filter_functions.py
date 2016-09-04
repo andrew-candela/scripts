99d8d457ca69725cee73072c6b69592972f9814f
andrew <andrewpcandela@gmail.com>
2016-09-03 17:13:56 -0700

    gave example of usage

--- book_club/spam_filter/filter_functions.py
+++ book_club/spam_filter/filter_functions.py
@@ -1,16 +1,19 @@
 import re
+from collections import defaultdict
+import math
+
 
 def tokenize(message):
 	message = message.lower() 
-	all_words=re.findall("[a-z0-9]+")
+	all_words=re.findall("[a-z0-9]+",message)
 	return set(all_words) 
 
 def count_words(training_set):
- 	"""training set consists of pairs (message, is_spam)"""
+ 	"""training set consists of pairs (message, is_spam)""" 	
 	counts = defaultdict(lambda: [0, 0])
 	for message, is_spam in training_set:
-	for word in tokenize(message):
-	counts[word][0 if is_spam else 1] += 1
+		for word in tokenize(message):
+			counts[word][0 if is_spam else 1] += 1
 	return counts
 
 def word_probabilities(counts, total_spams, total_non_spams, k=0.5):
@@ -35,4 +38,20 @@ def spam_probability(word_probs, message):
 	prob_if_spam = math.exp(log_prob_if_spam)
 	prob_if_not_spam = math.exp(log_prob_if_not_spam)
 	return prob_if_spam / (prob_if_spam + prob_if_not_spam)
+
+class NaiveBayesClassifier:
+	def __init__(self, k=0.5): 
+		self.k = k
+		self.word_probs = []
+	
+	def train(self, training_set):
+	    # count spam and non-spam messages
+		num_spams = len([is_spam for message, is_spam in training_set if is_spam])
+		num_non_spams = len(training_set) - num_spams
+		# run training data through our "pipeline"
+		word_counts = count_words(training_set)
+		self.word_probs = word_probabilities(word_counts, num_spams, num_non_spams, self.k)
+	def classify(self, message):
+		return spam_probability(self.word_probs, message)
+
 
