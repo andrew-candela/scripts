7e704c700dce699e83b6d04cf1959edf3e960608
andrew <andrewpcandela@gmail.com>
2016-09-18 07:47:13 -0700

    cleaned up KNN

--- book_club/k_nearest_neighbors/knn_weighted.py
+++ book_club/k_nearest_neighbors/knn_weighted.py
@@ -7,44 +7,37 @@ import pandas as pd
 #I'll have a target location - I want to guess the value of a target variable based on the values 
 #of other data points, weighted by distance from the target location
 
-#this takes a really long time to run. I'll bet there are all kinds of optimizations I can make
-
-#let's restict ourselves to a unit square
+#let's restict ourselves to a unit square - pick some random points
 x_pos=np.random.uniform(0,1,50)
 y_pos=np.random.uniform(0,1,50)
-
-#pick a random point
+#pick a random point as the target location 
 target_location=np.random.uniform(0,1,2)
-
 #pick some random values
 target_value=np.random.standard_normal(50)
-
 #build the dataframe
 dataset=pd.DataFrame(zip(x_pos,y_pos,target_value))
-dataset.columns=['x','y','val']
-dataset['distance']=np.sqrt(((dataset['x']-target_location[0])**2) + ((dataset['y']-target_location[1])**2))
 
-#now I have the value and the distance from the target point right in the data frame.
-#I need to do a little math to figure out the weights...
 
-#the condition is that w[i]/w[j] = distance[j]/distance[i]
-#or w[i]=w[min]*(d[min]*d[i])
+#define a function that takes the dataset and a target point, and returns the estimate
+def knn_weighted(data_df,point_to_estimate):
+	"""data will be a data frame with 3 columns - ['x','y','val']. Point to estimate is an array of length 2"""
+	data_df.columns=['x','y','val']
+	#build distances into data_df
+	data_df['distance']=np.sqrt(((data_df['x']-point_to_estimate[0])**2) + ((data_df['y']-point_to_estimate[1])**2))
+	#the condition is that w[i]/w[j] = distance[j]/distance[i]
+	#or w[i]=w[min]*(d[min]*d[i])
+	#impose the condition 1=w[min]*d[min]*sum(1/d[i])
+	#this will let us solve for w[min]
+	d_min=data_df.min()['distance']
+	data_df['recip_distance']=np.reciprocal(data_df['distance'])
+	weight_base=np.reciprocal(d_min * data_df.sum()['recip_distance'])
+	#now I know the initial weight, let's calculate the full weight vector
+	data_df['weight']=weight_base*d_min*np.reciprocal(data_df['distance'])	
+	#estimate value
+	data_df['weighted_scores']=data_df['val']*data_df['weight']
+	return data_df.sum()['weighted_scores']
 
-#impose the condition 1=w[min]*d[min]*sum(1/d[i])
-#this will let us solve for w[min]
 
-d_min=dataset.min()['distance']
-
-dataset['recip_distance']=np.reciprocal(dataset['distance'])
-weight_base=np.reciprocal(d_min * dataset.sum()['recip_distance'])
-
-#now I know the initial weight, let's calculate the full weight vector
-dataset['weight']=weight_base*d_min*np.reciprocal(dataset['distance'])
-
-#check if the wieghts sum to 1
-#print(dataset.sum()['weight'])
-
-#estimate value
-
-dataset['weighted_scores']=dataset['val']*dataset['weight']
-print(dataset.sum()['weighted_scores'])
+#run the script
+estimate=knn_weighted(dataset,target_location)
+print(estimate)
